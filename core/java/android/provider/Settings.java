53afff4d3200daf83445566e6f4ea812a3770ade
cristianomatos <cristianobmatos@gmail.com>
2016-12-07 14:50:41 +0800

    Recents FAB Clear All Button Tweaks [1/2]
    - Its a longway squash of multiple commits
    
    - Credits : @varund7726 , @xyyx , @0xD34D , @maniac103 , @cristianomatos , @altaf-mahdi , @temasek
    
    Change-Id: Ifd6601d4fc789693916e8cc0930c30ab9e148a35

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3889,7 +3889,18 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
-        
+        /**
+         * show clear all recents button
+         * @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
         /** @hide */
         public static final Validator KEY_VALIDATOR = new Validator() {
             @Override
