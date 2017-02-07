b14cfb4a3d23f27d2cd3a18aa030767a44ef8e94
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2017-02-07 20:04:12 +0100

    Add SlimDoze [1/2]
    Adds the following features for controlling Android's doze puls (aka. Ambient Display) :
    
    - doze pulse fade in time for pickup (actually for anything but double-tap-to-doze)
    - doze pulse fade in time for double-tap-to-doze
    - doze pulse timeout (duration)
    - doze pulse fade out time
    - doze pulse brightness level
    - pickup gesture trigger (trigger a doze pulse on device pickup)
    - double tap trigger (trigger a doze pulse on double-tapping screen while screen is turned off)
    - Notification trigger (trigger a doze pulse when a notification arrives)
    
    Note : options will vary depending on device features.
    
    --------
    
    Squashed commit of all the previous commits, based on my port of SlimDoze to YAOSP-NG :
    
    - adapted metrics to N
    
    - Gesturesettings: Remove AmbientDisplay bits, redundant with SlimDim (which has more options anyways)
    
    - Double tap to wake on doze
    
      Thanx to Patrick for this excellent idea and to Dankoman for pointing me in the right direction ;)
    
    - add double-tap to trigger doze pulse (this is disabled by default)
    
      This will add the Pixel's double-tap to trigger doze to any device with native double-tap-to-wake support.
      Double-tap-to-wake needs to be enabled for this to work !
      Huge thanks to Griffin Millender for the "how to trigger a doze pulse" help ! ;)
      (Note : shamu will need a kernel commit for this to wake)
    
    - add fade in / out (respect the different setting for fade in due to pickup or double-tap)
    
    Change-Id: Ica933d917e5ee8bb3f8e4d3147f81e621a116147
    Signed-off-by: dankoman <dankoman30@gmail.com>
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -112,6 +112,7 @@ import android.service.dreams.DreamService;
 import android.service.dreams.IDreamManager;
 import android.speech.RecognizerIntent;
 import android.telecom.TelecomManager;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
 import android.util.Log;
@@ -283,6 +284,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     static public final String SYSTEM_DIALOG_REASON_RECENT_APPS = "recentapps";
     static public final String SYSTEM_DIALOG_REASON_HOME_KEY = "homekey";
     static public final String SYSTEM_DIALOG_REASON_ASSIST = "assist";
+
+    // Double-tap-to-doze
+    private boolean mDoubleTapToWake;
+    private boolean mDoubleTapToDoze;
+    private boolean mNativeDoubleTapToDozeAvailable;
 
     /**
      * These are the system UI flags that, when changing, can cause the layout
@@ -1022,6 +1028,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.KEYGUARD_TOGGLE_TORCH), false, this,
                     UserHandle.USER_ALL);
+
+            // Double-tap-to-doze
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.DOUBLE_TAP_TO_WAKE), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOZE_TRIGGER_DOUBLETAP), false, this,
+                    UserHandle.USER_ALL);
+
             updateSettings();
         }
 
@@ -1978,6 +1993,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mHasFeatureWatch = mContext.getPackageManager().hasSystemFeature(FEATURE_WATCH);
         mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
 
+        // Double-tap-to-doze
+        mNativeDoubleTapToDozeAvailable = !TextUtils.isEmpty(
+                mContext.getResources().getString(R.string.config_dozeDoubleTapSensorType));
+
         // Init display burn-in protection
         boolean burnInProtectionEnabled = context.getResources().getBoolean(
                 com.android.internal.R.bool.config_enableBurnInProtection);
@@ -2494,6 +2513,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         ContentResolver resolver = mContext.getContentResolver();
         Resources resources = mContext.getResources();
         boolean updateRotation = false;
+
+        // Double-tap-to-doze
+        mDoubleTapToWake = Settings.Secure.getInt(resolver,
+                Settings.Secure.DOUBLE_TAP_TO_WAKE, 0) == 1;
+        mDoubleTapToDoze = Settings.System.getInt(resolver,
+                Settings.System.DOZE_TRIGGER_DOUBLETAP, 0) == 1;
+
         synchronized (mLock) {
             mEndcallBehavior = Settings.System.getIntForUser(resolver,
                     Settings.System.END_BUTTON_BEHAVIOR,
@@ -7004,7 +7030,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             case KeyEvent.KEYCODE_WAKEUP: {
                 result &= ~ACTION_PASS_TO_USER;
-                isWakeKey = true;
+                // Double-tap-to-doze
+                if (mDoubleTapToWake && mDoubleTapToDoze && !mNativeDoubleTapToDozeAvailable) {
+                    isWakeKey = false;
+                    if (!down) {
+                        mContext.sendBroadcast(new Intent("com.android.systemui.doze.pulse"));
+                    }
+                } else {
+                    isWakeKey = true;
+                }
                 break;
             }
 
